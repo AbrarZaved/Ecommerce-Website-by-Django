217aad7a753d975fe6b3c9342475012a45bd01ad
AbrarZaved <abrarzaved2002@gmail.com>
2025-01-10 00:41:32 +0600

    Add dashboard app with initial models, views, and templates

---
+++ static/js/index.js
@@ -0,0 +1,34 @@
+console.log("Hello");
+
+document.addEventListener("DOMContentLoaded", hover_effect);
+
+function hover_effect() {
+  var categories = document.querySelector("[name='dropdown']");
+  var dropdownMenu = document.querySelector(".dropdown-menu");
+
+  categories.addEventListener("mouseover", function () {
+    console.log("hovered");
+    categories.classList.add("show");
+    dropdownMenu.classList.add("show");
+    categories.setAttribute("aria-expanded", "true");
+  });
+
+  categories.addEventListener("mouseout", function () {
+    categories.classList.remove("show");
+    dropdownMenu.classList.remove("show");
+    categories.setAttribute("aria-expanded", "false");
+  });
+
+  dropdownMenu.addEventListener("mouseover", function () {
+    categories.classList.add("show");
+    dropdownMenu.classList.add("show");
+    categories.setAttribute("aria-expanded", "true");
+  });
+
+  dropdownMenu.addEventListener("mouseout", function () {
+    categories.classList.remove("show");
+    dropdownMenu.classList.remove("show");
+    categories.setAttribute("aria-expanded", "false");
+  });
+}
+
